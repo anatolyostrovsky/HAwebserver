eca86b41be0632ef445e38ed1b76b7ace4ee5d35
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2024-06-05 19:26:18 +0100

    user data IP added

--- user-data.sh
+++ user-data.sh
@@ -4,10 +4,10 @@ sudo yum update
 sudo yum install nginx -y
 sudo systemctl start nginx
 
-myip='curl checkip.amazonaws.com'
+myip=`curl checkip.amazonaws.com`
 
-chmod 2775 /usr/share/nginx/html 
-find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
-find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
+sudo chmod 2775 /usr/share/nginx/html 
+sudo find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
+sudo find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
 
-echo "<h3>WebServer with IP: $myip</h2> Made with Terraform </h3>" > /usr/share/nginx/html/index.html
+sudo echo "<h3>WebServer with IP: $myip Made with Terraform </h3>" > /usr/share/nginx/html/index.html
